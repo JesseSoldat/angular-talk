54ca00dc721bdc82b0e3852918fd444fe68eb397
JesseSoldat <jsoldat@hotmail.com>
2017-12-28 18:17:47 +0700

    create an image for welcome page

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   uid = null;
+  subscription: Subscription;
 
   constructor(private router: Router,
               private authService: AuthService) {}
 
   ngOnInit() {
     this.authService.getUser();
-    
-    this.authService.uid.subscribe(uid => {
+
+    this.subscription = this.authService.uid.subscribe(uid => {
       this.uid = uid; 
       if(this.uid === null) {
         this.router.navigate(['']);
@@ -33,6 +35,10 @@ export class HeaderComponent implements OnInit {
   onLogout(e) {
     e.preventDefault();
     this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
