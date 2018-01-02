30330cdc31ce7f16f2d6eb1fd4f149abb7eccbc5
JesseSoldat <jsoldat@hotmail.com>
2018-01-02 19:01:27 +0700

    added fade-in animation

--- src/app/core/message/message.component.ts
+++ src/app/core/message/message.component.ts
@@ -5,11 +5,13 @@ import Alert from '../../models/alert';
 import { alertMessages } from '../../models/message.enum';
 import { Subscription } from 'rxjs/Subscription';
 import { capitalize } from '../../shared/helper-functions';
+import { fadeinTrigger } from '../../shared/animations/fadein.animation';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss']
+  styleUrls: ['./message.component.scss'],
+  animations: [fadeinTrigger ]
 })
 export class MessageComponent implements OnInit {
   alert$: Observable<Alert>;
---
+++ src/app/shared/animations/fadein.animation.ts
@@ -0,0 +1,23 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+export const fadeinTrigger = trigger('fadein', [
+
+  transition('void => *', [
+    style({
+      opacity: 0
+    }),
+    animate('900ms ease-in', style({
+      opacity: 1
+    })),
+    animate('900ms ease-in')
+  ]),
+  transition('* => void', [
+    style({
+      opacity: 1
+    }),
+    animate('900ms ease-out', style({
+      opacity: 0
+    })),
+
+  ])
+]);
